b69add28dbffa97e7e147814ea936d033b4eb34b
GAYOUNG <equalslee@tpcg.co.kr>
2024-12-06 18:30:46 +0900

    Style: dashboard laout 디자인 수정

--- demo-front/src/app/routes/app/DashboardLayout .tsx
+++ demo-front/src/app/routes/app/DashboardLayout .tsx
@@ -1,16 +1,14 @@
-import {
-  FileTextOutlined,
-  InfoCircleOutlined,
-  MessageOutlined,
-  QuestionCircleOutlined,
-} from "@ant-design/icons";
-import { Layout, Menu, Card, message, Typography, Col } from "antd";
+import { Layout, Menu, message, Typography, Col } from "antd";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import DK_LOGO from "@/assets/dk_logo.png";
-import TPCG_G from "@/assets/tpcg_google.png";
+import TPCG_G from "@/assets/google.png";
 import "@/styles/layout.scss";
 import { Footer } from "antd/es/layout/layout";
 import { useEffect, useState } from "react";
+import { AiOutlineComment, AiOutlineDashboard } from "react-icons/ai";
+import { MdOutlineMedicalServices } from "react-icons/md";
+import { TbBook2 } from "react-icons/tb";
+import { FaInfo, FaRegCircleQuestion } from "react-icons/fa6";
 
 const { Header, Sider, Content } = Layout;
 const { Title, Text } = Typography;
@@ -19,25 +17,37 @@ const items = [
   {
     key: "d-note",
     label: "D-Note",
-    icon: <FileTextOutlined />,
-    status: "inactive",
+    icon: <TbBook2 size={20} />,
+    status: "active",
+    children: [
+      {
+        key: "medical/dashboard",
+        label: <p>경과기록지 작성</p>,
+        icon: <AiOutlineDashboard size={20} />,
+      },
+      {
+        key: "medical/assistant",
+        label: <p>진료상담 요약</p>,
+        icon: <MdOutlineMedicalServices size={20} />,
+      },
+    ],
   },
   {
     key: "d-chat",
     label: "D-Chat",
-    icon: <MessageOutlined />,
+    icon: <AiOutlineComment size={20} />,
     status: "active",
   },
   {
     key: "d-qna",
     label: "D-Qna",
-    icon: <QuestionCircleOutlined />,
+    icon: <FaRegCircleQuestion size={20} />,
     status: "inactive",
   },
   {
     key: "d-inq",
     label: "D-INQ",
-    icon: <InfoCircleOutlined />,
+    icon: <FaInfo size={20} />,
     status: "inactive",
   },
 ];
@@ -87,7 +97,9 @@ export const DashboardLayout = () => {
               display: "flex",
               alignItems: "flex-end",
             }}
-            onClick={() => navigate("/")}
+            onClick={() => {
+              navigate("/");
+            }}
           >
             <img src={DK_LOGO} alt="Logo" style={{ height: "30px" }} />
           </div>
@@ -119,119 +131,26 @@ export const DashboardLayout = () => {
         </div>
       </Header>
       <Layout className="layout">
-        <Sider theme="light" width={140}>
-          <Menu
-            className="menu-wrapper"
-            mode="inline"
-            theme="light"
-            defaultSelectedKeys={["/dashboard/d-note"]}
-            items={items}
-            onClick={onMenuClick}
-          />
+        <Sider className="custom-sider" width={240}>
+          <div className="menu-wrapper">
+            <h1>MAIN MENU</h1>
+            <Menu
+              mode="inline"
+              className="custom-menu"
+              defaultSelectedKeys={["/dashboard/d-note"]}
+              items={items}
+              onClick={onMenuClick}
+            />
+          </div>
         </Sider>
         <Content className="content">
           <Outlet />
         </Content>
-        <Sider width={270} className="task-sider">
-          <Card
-            style={{
-              minHeight: "100%",
-              maxHeight: "calc(100vh - 160px)",
-              overflowY: "auto",
-            }}
-            bordered={false}
-          >
-            <div
-              style={{
-                marginBottom: "20px",
-                fontWeight: "bold",
-                fontSize: "16px",
-              }}
-            >
-              사용자 관심분야
-            </div>
-            <div style={{ marginBottom: "20px" }}>
-              <select
-                style={{ width: "100%", padding: "5px", fontSize: "14px" }}
-              >
-                <option>일반</option>
-                <option>보험심사 관련규정</option>
-                <option>간호자격심사 관련 규정</option>
-                <option>표준진료 관련 규정</option>
-              </select>
-            </div>
-
-            <div
-              style={{
-                marginBottom: "10px",
-                fontWeight: "bold",
-                fontSize: "16px",
-              }}
-            >
-              보험심사관련규정
-            </div>
-
-            <ul
-              style={{
-                marginBottom: "10px",
-                paddingLeft: "20px",
-                fontSize: "14px",
-              }}
-            >
-              <li>요양급여의 적용기준 및 방법에 관한 세부사항 [링크]</li>
-              <li>요양급여의 적용기준 및 방법에 관한 세부사항 (약제) [링크]</li>
-            </ul>
-
-            <div
-              style={{
-                marginBottom: "10px",
-                fontWeight: "bold",
-                fontSize: "16px",
-              }}
-            >
-              자주하는 질문:
-            </div>
-            <ul
-              style={{
-                marginBottom: "10px",
-                paddingLeft: "20px",
-                fontSize: "14px",
-              }}
-            >
-              <li>최근 고시 목록</li>
-              <li>예정 고시 목록</li>
-              <li>응급의료수가 관련 ...</li>
-            </ul>
-
-            <div
-              style={{
-                marginBottom: "10px",
-                fontWeight: "bold",
-                fontSize: "16px",
-              }}
-            >
-              사용자정의의 자주하는 질문:
-            </div>
-            <ul
-              style={{
-                marginBottom: "10px",
-                paddingLeft: "20px",
-                fontSize: "14px",
-              }}
-            >
-              <li>최근 고시 목록</li>
-              <li>예정 고시 목록</li>
-              <li>응급의료수가 관련 ...</li>
-            </ul>
-
-            <div style={{ fontWeight: "bold", fontSize: "16px" }}>용어사전</div>
-          </Card>
-        </Sider>
       </Layout>
       <Footer
         style={{
           backgroundColor: "white",
-          padding: "15px 0",
+          padding: "10px 0",
           borderTop: "2px solid #f7f8fa",
           textAlign: "center",
         }}
@@ -239,7 +158,7 @@ export const DashboardLayout = () => {
         <Col>
           <Text
             style={{
-              fontSize: "13px",
+              fontSize: "12px",
               fontWeight: "900",
               textAlign: "center",
             }}
@@ -248,7 +167,7 @@ export const DashboardLayout = () => {
             <span style={{ color: "#82B8AC", fontWeight: "bold" }}>
               CG&nbsp;
             </span>
-            <Text style={{ fontSize: "12px", color: "black" }}>
+            <Text style={{ fontSize: "11px", color: "black" }}>
               ALL RIGHTS RESERVED
             </Text>
           </Text>
